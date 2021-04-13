cfd31b7f4c90023d9dcf6a800ce8686bf8438c48
Lucas Contreras <lucascontre95@gmail.com>
2021-04-13 07:34:26 -0300

    ContactRepository ?

--- pkg/gateways/storage/contact_repository.go
+++ pkg/gateways/storage/contact_repository.go
@@ -5,8 +5,27 @@ import (
 	"gorm.io/gorm"
 )
 
-type ContactRepo struct {
+type ContactModel struct {
 	gorm.Model
 	Name  string
 	Phone string
+}
+type ContactRepo struct {
+	db *gorm.DB
+}
+
+func NewContactRepository() ContactRepo {
+	db, err := gorm.Open(sqlite.Open("ims.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&ContactModel{})
+	return ContactRepo{db}
+}
+
+func (cr *ContactRepo) Add(name, phone string) {
+	cr.db.Create(&ContactModel{
+		Name:  name,
+		Phone: phone,
+	})
 }
